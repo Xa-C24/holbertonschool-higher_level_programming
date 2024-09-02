13e3e8a826012323c4835988d52dee0f336b9187
Xa-C24 <9335@holbertonstudents.com>
2024-09-02 15:24:08 +0200

    modification code

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming with Python " 
-print(str1)
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+print((str[39:66] + " " + str[106:110] + " " + str[:6]))
