e3fd9badd59b29c5570a1ef3762d527e7327806e
Xa-C24 <9335@holbertonstudents.com>
2024-09-06 11:23:57 +0200

    Write a program that imports all functions from the file calculator_1.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
+from sys import argv
 from calculator_1 import add, sub, mul, div
-import sys
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
     try:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
+        a = int(argv[1])
+        b = int(argv[3])
     except ValueError:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    operator = sys.argv[2]
+    operator = argv[2]
 
     if operator == "+":
         result = add(a, b)
@@ -25,10 +24,10 @@ if __name__ == "__main__":
     elif operator == "/":
         if b == 0:
             print("Error: Division by zero")
-            sys.exit(1)
+            exit(1)
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
 print(f"{a} {operator} {b} = {result}")
