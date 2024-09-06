3b6cb9c44d5d7bb69c1d9e2337b822ec851dda4d
Xa-C24 <9335@holbertonstudents.com>
2024-09-06 10:55:32 +0200

    Write a program that imports all functions from the file calculator_1.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -18,6 +18,9 @@ if __name__ == "__main__":
     elif operator == "*":
         result = mul(a, b)
     elif operator == "/":
+        if b == 0:
+            print("Error: Division by zero")
+            sys.exit(1)
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
