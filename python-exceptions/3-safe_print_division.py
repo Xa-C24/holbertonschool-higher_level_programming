e5cc5fb059eafcc567de82ed72dc62ee8d428d23
Xa-C24 <9335@holbertonstudents.com>
2024-09-11 12:21:03 +0200

    Rectifiation Code

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
-        calcul = a / b
-        print("Result: {:.1f}".format(calcul))
-        return calcul
-    except:
-        ZeroDivisionError
-        print("Result: None")
-        return None
+        result = a / b
+    except ZeroDivisionError:
+        calcul = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
+    
