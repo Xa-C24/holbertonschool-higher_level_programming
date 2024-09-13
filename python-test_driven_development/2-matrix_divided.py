312ce8bef904fbd94687876aa704b1c450c23f44
Xa-C24 <9335@holbertonstudents.com>
2024-09-13 11:08:34 +0200

    file .py for divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,22 +2,26 @@
 """
 Module for dividing all elements of a matrix by a divisor
 """
+
+
 def matrix_divided(matrix, div):
     if matrix == [] or matrix == [[]]:
         return []
-    
-    if not isinstance(matrix, list) or len(matrix) == 0 or not all (isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not isinstance(matrix, list) or len(matrix) == 0 or not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     row_size = len(matrix[0])
     if not all(len(row) == row_size for row in matrix):
-        raise TypeError ("Each row of the matrix must have the same size")
-    
+        raise TypeError("Each row of the matrix must have the same size")
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
-    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+
+    new_matrix = [[round(element / div, 2) for element in row]
+                  for row in matrix]
 
     return new_matrix
