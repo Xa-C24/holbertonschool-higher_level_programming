1968f8da66da56bbd185355e24ee055300064912
Xa-C24 <9335@holbertonstudents.com>
2024-09-13 10:36:14 +0200

    file .py for divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,7 +16,7 @@ def matrix_divided(matrix, div):
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError("divison by zero")
+        raise ZeroDivisionError("division by zero")
     
     new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
 
