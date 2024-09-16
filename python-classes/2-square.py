93e8a2fc0909e2eb629cb121eec61c7b5d81eb33
Xa-C24 <9335@holbertonstudents.com>
2024-09-16 14:37:05 +0200

    Write a class Square that defines a square, add isinstance if TypeError/ValueError.

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Module 2-square: défine a class Square with size validation"""
+"""Module 2-square: défine a class Square with size validation."""
 
 
 class Square:
-    """Square class"""
+    """Square class."""
 
     def __init__(self, size=0):
 
         if not isinstance(size, int):
             raise TypeError("Size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >=0")
 
