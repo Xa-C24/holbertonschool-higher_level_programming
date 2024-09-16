df984efaec2ba72cca3693703cab8903f15411d7
Xa-C24 <9335@holbertonstudents.com>
2024-09-16 14:30:18 +0200

    Write a class Square that defines a square, add area fonction

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 2-square: défine a class Square with size validation"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >=0")
+        
+        self.__size = size
+    
+    def area(self):
+        """Calculate and return area of square"""
+
+        return self.__size **2
