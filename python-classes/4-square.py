3c19a2709980153fd427482df7bef6dbca72571a
Xa-C24 <9335@holbertonstudents.com>
2024-09-16 15:22:40 +0200

    Write a class Square that defines a square, add getter/setter.

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module 4-square: défine a class Square with size validation, getter, setter."""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+
+        self.size = size
+
+    @property
+    def size(self):
+        """" Getter for the size attribute."""
+
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute."""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return area of square"""
+
+        return self.__size ** 2
