2f7b62451fd4f2d0d32631567b40991f90bd9fc2
Xa-C24 <9335@holbertonstudents.com>
2024-09-18 15:23:30 +0200

    file .py for test file 4-print_square.txt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,6 +2,8 @@
 def print_square(size):
 
     if not isinstance(size, int):
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
