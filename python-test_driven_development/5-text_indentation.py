34ab9b8290eeb5cf964cadb28274c86dd706a2c8
Xa-C24 <9335@holbertonstudents.com>
2024-09-19 09:48:21 +0200

    file.py with the file 5.txt for test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,6 +5,16 @@ Module that defines a function text_indentation.
 
 
 def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: '.', '?', and ':'.
+
+    Args:
+        text (str): The input text to format.
+
+    Raises:
+        TypeError: If the input text is not a string.
+    """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
