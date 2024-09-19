f15332f2c2b0cb027bcdb7516eac8771cffea2cd
Xa-C24 <9335@holbertonstudents.com>
2024-09-19 09:28:17 +0200

    file.py with the file 5.txt for test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module that defines a function text_indentation.
+"""
+
+
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
@@ -6,16 +11,13 @@ def text_indentation(text):
     ignore_space = False
 
     for i in range(len(text)):
-        
+
         if ignore_space and text[i] == ' ':
             continue
         ignore_space = False
-        
-       
+
         print(text[i], end="")
 
-        
         if text[i] in ['.', '?', ':']:
             print("\n")
             ignore_space = True
-            
