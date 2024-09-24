9726136069727291ffc323c32d4fa57bb0946f09
Xa-C24 <9335@holbertonstudents.com>
2024-09-24 09:56:19 +0200

    add documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module defines a function called lookup
+"""
+
 def lookup(obj):
     """
         Returns a list of the attributes and methods available for an object.
