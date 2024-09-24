a33a25382c37522897c6210367fde55677f237c9
Xa-C24 <9335@holbertonstudents.com>
2024-09-24 10:59:15 +0200

    add documentation

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 class BaseGeometry:
+    """
+    This module defines a class BaseGeometry that includes a method area.
+
+    Class:
+        BaseGeometry: Class representing the basic geometry.
+
+    Methods:
+        area(self): Lifts an exception.
+        """
     class BaseGeometry:
         """class BaseGeometry with an area method."""
 
