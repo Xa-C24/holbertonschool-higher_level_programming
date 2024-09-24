82ef9a7db1631a7f8591570134221b1d85f56107
Xa-C24 <9335@holbertonstudents.com>
2024-09-24 13:32:40 +0200

    add documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""4-inherits_from.py
 """
+    This module check the inheritance of an object
+"""
+
 
 def inherits_from(obj, a_class):
     """ Check for direct or indirect inheritance """
-    
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
