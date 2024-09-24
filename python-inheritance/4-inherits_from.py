df556573aa828afdfdbcaa970d36f9d16f0edf36
Xa-C24 <9335@holbertonstudents.com>
2024-09-24 10:40:23 +0200

    add documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-  """ 
-  Returns True if the object is an instance of a class that inherits from the specified class, 
-  but not if the object is a direct instance of the specified class. 
-  """
-  return isinstance(obj, a_class) and type(obj) != a_class
+    """ 
+    This module define a function which checks whether an object 
+    is an instance of a class that inherits from the specified class.
+  
+    Args:
+        obj: The object to be checked.
+        a_class: The reference class for the comparison.
+  
+    Returns: 
+        True if the object is an instance of a class that inherits from the specified class, 
+        but not if the object is a direct instance of the specified class. 
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
