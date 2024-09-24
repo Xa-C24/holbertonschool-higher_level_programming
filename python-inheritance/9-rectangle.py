b0f1d41dcabfca9b16a16827b61b177a1acde0e9
Xa-C24 <9335@holbertonstudents.com>
2024-09-24 12:06:06 +0200

    add documentation

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """class BaseGeometry with an area method."""
+"""9-rectangle.py
+"""
 
-    def area(self):
-        raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Classe Rectangle qui hérite de BaseGeometry."""
+    """Rectangle class that inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Initialise le rectangle avec une largeur et une hauteur."""
+        """initializes the rectangle with a width and a height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """Retourne l'aire du rectangle."""
+        """Return area of rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """Retourne une représentation lisible de l'objet Rectangle."""
+        """"Returns a readable representation of the Rectangle object"""
         return f"[Rectangle] {self.__width}/{self.__height}"
