3e6af7e2d1c77d9294b900b47855c6f0bcdaa71d
Xa-C24 <9335@holbertonstudents.com>
2024-10-01 09:18:30 +0200

    Write a function that creates an Object

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""import_load_json Module"""
 import json
-"""function for add function json"""
+
 
 def load_from_json_file(filename):
     """Creates an object from a JSON file."""
