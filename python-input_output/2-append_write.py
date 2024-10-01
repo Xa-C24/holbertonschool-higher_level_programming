534f09ddecd76bf7198a6d2507bfb0d0c80a000a
Xa-C24 <9335@holbertonstudents.com>
2024-10-01 12:04:20 +0200

    Write a function that appends a string at the end of a text file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""append_file"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF8) and returns the number."""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
