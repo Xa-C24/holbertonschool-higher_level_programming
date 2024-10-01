bc8bf91e3e1ab5602d81ea3d5427343cb6e8a542
Xa-C24 <9335@holbertonstudents.com>
2024-10-01 15:56:24 +0200

    Write a class Student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """"Initialise a new student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
