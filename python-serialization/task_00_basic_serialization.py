7028ff0da0283ae8098065e0188f8d3796196b9a
Xa-C24 <9335@holbertonstudents.com>
2024-10-03 15:49:21 +0200

    Create a basic serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+"""Import Module json"""
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialises the 'data' dictionary in JSON and saves it in the 'filename' file."""
+
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    """Loads data from the 'filename' file and deserializes it into a Python dictionary."""
+
+    with open(filename, 'r') as file:
+        json.load(filename)
