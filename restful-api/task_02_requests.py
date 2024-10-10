b4c8dfc0df923efb811c695a1f626667f1926abe
Xa-C24 <9335@holbertonstudents.com>
2024-10-10 13:17:47 +0200

    creat simple API

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Import requests and csv module"""
+
+'''Module task_02 request: Define consuming and
+processing data from an API using python'''
 
 
 import requests
@@ -7,35 +9,39 @@ import csv
 
 
 def fetch_and_print_posts():
-    """Function for retrieving and displaying post titles"""
+    '''Function for retrieving and displaying post titles'''
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status Code: {response.status_code}")
 
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
-        for post in response.json():
+        posts = response.json()
+        for post in posts:
             print(post["title"])
     else:
         print("Failed to retrieve posts.")
 
 
 def fetch_and_save_posts():
-    """Function for retrieving and saving posts in a CSV file"""
+    '''Function for retrieving and saving posts in a CSV file'''
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
     if response.status_code == 200:
         posts = response.json()
         posts_data = []
+
         for post in posts:
-            posts_data.append(
-                {'id': post['id'], 'title': post['title'],
-                  'body': post['body']} for post in response.json())
+            posts_data.append({
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']}
+            )
 
         with open("post.csv", "w", newline="") as file:
             writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
             writer.writeheader()
-            writer.writerows(posts)
+            writer.writerows(posts_data)
 
         print("Posts saved to post.csv")
-    
+
     else:
         print("Failed to retrieve posts")
