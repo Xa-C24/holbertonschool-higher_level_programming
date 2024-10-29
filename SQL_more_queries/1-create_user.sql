6ed4daef2820148f54d6d8f33e6cd834b44c03fa
Xa-C24 <9335@holbertonstudents.com>
2024-10-29 14:10:10 +0100

    Add .gitignore to exclude __pycache__ and other temporary files

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- script that creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1' @localhost IDENDIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1' @localhost IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Accorder tous les privilèges sur le serveur MySQL à l'utilisateur
 GRANT ALL PRIVILEGES on *.* TO  'user_0d_1'@'localhost';
