5d1f16ebe2ba6202758aeb26a11831b71424e3f3
Xa-C24 <9335@holbertonstudents.com>
2024-10-29 14:16:30 +0100

    script that creates the MySQL server user user_0d_1

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2' @localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Accorder tous les privilèges sur le serveur MySQL à l'utilisateur
-GRANT SELECT ON hbtn_0d_2* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2*. TO 'user_0d_2'@'localhost';
 
 -- Appliquer les changements
 FLUSH PRIVILEGES;
