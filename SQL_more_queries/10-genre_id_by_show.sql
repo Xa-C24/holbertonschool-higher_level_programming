82520369592b145b0c9d09d8ef25862c592e6559
Xa-C24 <9335@holbertonstudents.com>
2024-10-29 16:49:15 +0100

    Write a script that lists all shows contained in hbtn_0d_tvshows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,8 @@
 -- Import the database dump from hbtn_0d_tvshows
+-- Select the title of each TV show and the associated genre ID
+-- From the tables tv_shows and tv_show_genres
+-- Link the two tables by matching the TV show ID in both tables
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
