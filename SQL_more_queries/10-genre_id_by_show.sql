6b9bfeac35a39d3b0eddec3fdec15965eb27349e
Xa-C24 <9335@holbertonstudents.com>
2024-10-29 17:05:22 +0100

    Import the database dump from hbtn_0d_tvshows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,5 @@
--- Import the database dump from hbtn_0d_tvshows
--- Select the title of each TV show and the associated genre ID
--- From the tables tv_shows and tv_show_genres
--- Link the two tables by matching the TV show ID in both tables
+-- Script to list all shows with at least one associated genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
