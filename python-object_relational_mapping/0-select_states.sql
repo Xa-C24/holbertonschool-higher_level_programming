8b29b7a2b1a73ae91a9f85a95533290e12b047c7
Xa-C24 <9335@holbertonstudents.com>
2024-10-30 14:26:20 +0100

    Create states table in hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.sql
+++ python-object_relational_mapping/0-select_states.sql
@@ -2,6 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
 USE hbtn_0e_0_usa;
 
+-- Vider la table states si elle existe déjà
+TRUNCATE TABLE states;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
