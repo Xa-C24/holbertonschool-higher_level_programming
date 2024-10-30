9fae5c1fccb97116bca81bc188033df03003bc1f
Xa-C24 <9335@holbertonstudents.com>
2024-10-30 17:12:21 +0100

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Filtre les états en fonction de l'entrée utilisateur"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Arguments fournis en ligne de commande
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connexion a la base de données MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Création d'un curseur pour excuter les requetes
+    cursor = db.cursor()
+
+    # Requete pour filtrer les états en fonction du nom
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    # Récupération et affichage des résultats
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Fermeture du curseur et de la connexion
+    cursor.close()
+    db.close()
