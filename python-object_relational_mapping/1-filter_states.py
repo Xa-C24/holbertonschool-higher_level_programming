bf4bedd82bc83c975d48def7f0f713bd2ad0f43c
Xa-C24 <9335@holbertonstudents.com>
2024-10-30 16:51:12 +0100

    script that lists all states with a name starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,26 +10,26 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-# Connexion a la base de données MySQL
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=mysql_username,
-    passwd=mysql_password,
-    db=database_name
-)
+    # Connexion a la base de données MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
 
-# Création d'un curseur pour excuter les requetes
-cursor = db.cursor()
+    # Création d'un curseur pour excuter les requetes
+    cursor = db.cursor()
 
-# Requete pour récupérer tous les états commençant par "N"
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY ID ASC")
+    # Requete pour récupérer tous les états commençant par "N"
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY ID ASC")
 
-# Récupération et affichage des résultats
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    # Récupération et affichage des résultats
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-# Fermeture du curseur et de la connexion
-cursor.close()
-db.close()
+    # Fermeture du curseur et de la connexion
+    cursor.close()
+    db.close()
