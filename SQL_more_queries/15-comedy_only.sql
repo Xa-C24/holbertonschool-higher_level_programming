fea560a7d146bc4201ecf320eeaa2a877950bc67
Xa-C24 <9335@holbertonstudents.com>
2024-10-30 16:20:29 +0100

    script that lists all Comedy shows in the database

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
--- script that lists all Comedy shows in the database
+-- Script qui liste tous les shows de genre Comedy dans la base de données
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
