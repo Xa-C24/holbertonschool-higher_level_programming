5c4f8e0ff0b03c53047788de39ad04c035c28de6
Xa-C24 <9335@holbertonstudents.com>
2024-10-30 16:01:41 +0100

    just comedy

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- script that lists all Comedy shows in the database
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id = (
+    SELECT id FROM tv_genres WHERE name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
