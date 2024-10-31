fd4a841ffd1fc786c8007af9614116fded36e87c
Xa-C24 <9335@holbertonstudents.com>
2024-10-31 13:06:44 +0100

    script that takes in arguments and displays all values in the states table

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+"""
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa, safe from injection
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Arguments fournis en ligne de commande
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connexion à la base de données MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+# Création d'un curseur pour exécuter les requêtes
+    cursor = db.cursor()
+
+    # Requête pour filtrer les états en fonction du nom
+    cursor.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC")
+
+    # Récupération et affichage des résultats
+    rows = cursor.fetchall()
+    for state in rows:
+        print(state)
+
+# Fermeture du curseur et de la connexion
+    cursor.close()
+    db.close()
