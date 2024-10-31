1655d59ad683f239bb3bccc645c15a90687f52d7
Xa-C24 <9335@holbertonstudents.com>
2024-10-31 08:56:01 +0100

    battle with pycodestyle

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -5,4 +5,4 @@ JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id = (
     SELECT id FROM tv_genres WHERE name = 'Comedy'
 )
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC
