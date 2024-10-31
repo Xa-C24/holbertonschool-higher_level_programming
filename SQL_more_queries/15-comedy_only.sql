74982548cea5566d09e6bd15b1ddb708063cdf25
Xa-C24 <9335@holbertonstudents.com>
2024-10-31 08:59:15 +0100

    battle with pycodestyle

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,6 @@
 -- Script qui liste tous les shows de genre Comedy dans la base de données
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id = (
-    SELECT id FROM tv_genres WHERE name = 'Comedy'
-)
-ORDER BY tv_shows.title ASC
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
